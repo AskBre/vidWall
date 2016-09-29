9a573c47f1a9a71c903bc7da2322551fb2006ccc
Ask Brean <askvb@online.no>
2016-09-30 00:15:36 +0200

    add or remove screens with arrows

--- src/ofApp.cpp
+++ src/ofApp.cpp
@@ -3,7 +3,7 @@
 //--------------------------------------------------------------
 void ofApp::setup() {
 	ofBackground(0);
-	ofSetDataPathRoot("../Resources/data");
+//	ofSetDataPathRoot("../Resources/data");
 	//TODO BUG If squares are ontop of each other, they move together
 	int nVidSqrs(4);
 	for(int i=0; i<nVidSqrs; i++) {
@@ -17,67 +17,74 @@ void ofApp::setup() {
 	vidSqrs.back().setSource(CAM);
 }
 
-//--------------------------------------------------------------
 void ofApp::update(){
 	for(auto &v : vidSqrs) v.update();
 }
 
-//--------------------------------------------------------------
 void ofApp::draw(){
 	for(auto &v : vidSqrs) v.draw();
 }
-
 //--------------------------------------------------------------
-void ofApp::keyPressed(int key){
-
+void ofApp::addScreen() {
+	ofVec2f p(ofGetWidth()*(vidSqrs.size()+1), ofGetHeight()*0.5);
+	VideoSquare v;
+	v.setup(p);
+	vidSqrs.push_back(v);
 }
 
+void ofApp::removeScreen() {
+	vidSqrs.pop_back();
+}
 //--------------------------------------------------------------
+void ofApp::keyPressed(int key){
+	switch(key) {
+		case 'OF_KEY_RIGHT':
+			addScreen();
+			break;
+		case 'OF_KEY_LEFT':
+			removeScreen();
+			break;
+		default:
+			break;
+	}
+}
+
 void ofApp::keyReleased(int key){
 
 }
 
-//--------------------------------------------------------------
 void ofApp::mouseMoved(int x, int y){
 
 }
 
-//--------------------------------------------------------------
 void ofApp::mouseDragged(int x, int y, int button){
 
 }
 
-//--------------------------------------------------------------
 void ofApp::mousePressed(int x, int y, int button){
 
 }
 
-//--------------------------------------------------------------
 void ofApp::mouseReleased(int x, int y, int button){
 	for(auto &v : vidSqrs) v.mouseReleased(x, y, button);
 }
 
-//--------------------------------------------------------------
 void ofApp::mouseEntered(int x, int y){
 
 }
 
-//--------------------------------------------------------------
 void ofApp::mouseExited(int x, int y){
 
 }
 
-//--------------------------------------------------------------
 void ofApp::windowResized(int w, int h){
 
 }
 
-//--------------------------------------------------------------
 void ofApp::gotMessage(ofMessage msg){
 
 }
 
-//--------------------------------------------------------------
 void ofApp::dragEvent(ofDragInfo dragInfo){
 
 }
